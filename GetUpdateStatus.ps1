da189e01887e407eee5aa4173488dd1f05df8116
wleksono <108651008+wleksono@users.noreply.github.com>
2022-10-07 16:15:10 +1100

    Update GetUpdateStatus.ps1

--- GetUpdateStatus.ps1
+++ GetUpdateStatus.ps1
@@ -1,17 +1,11 @@
 $Sysinfo = New-Object -ComObject Microsoft.Update.SystemInfo
 $pending = $Sysinfo.RebootRequired
-if ($pending){
-	try{
-	    shutdown.exe /r /f /t 120
-	}
-	catch{
-	    exit 1
-	}
-	exit 2
-}
+if ($pending){return "Pending Reboot"}
 
 $Session = [activator]::CreateInstance([type]::GetTypeFromProgID("Microsoft.Update.Session"))#,$Computer))
 $UpdateSearcher = $Session.CreateUpdateSearcher()
+$TotalHistoryCount = $UpdateSearcher.GetTotalHistoryCount()
+$UpdateHistory = $UpdateSearcher.QueryHistory(0,$TotalHistoryCount)
 
 $Criteria = "IsHidden=0 and IsInstalled=0 and IsAssigned=1"
 
@@ -19,63 +13,29 @@ try{
     $SearchResult = $UpdateSearcher.Search($Criteria).Updates
 }
 catch{
-    exit 1
+    return "Update Search Failed"
 }
+
+$FailedUpdates = @()
 
 if($SearchResult.count -ne 0){
     foreach ($entry in $SearchResult){
-	    if ($entry.isDownloaded -eq $false){		
-		    $updateSession = New-Object -ComObject 'Microsoft.Update.Session'
-		    $updatesToDownload = New-Object -ComObject 'Microsoft.Update.UpdateColl'
-		    $updatesToDownload.Add($entry) | Out-Null
-            $downloader = $updateSession.CreateUpdateDownloader()
-            $downloader.Updates = $updatesToDownload
-		    try{
-        	    $downloadResult = $downloader.Download()
-		    }
-		    catch{
-		        "Download failed"
-		    }
-		    if ($downloadResult.ResultCode -eq 2) {
-			    $updatesToInstall = New-Object -ComObject 'Microsoft.Update.UpdateColl'
-			    $updatesToInstall.Add($entry) | Out-Null
-			
-			    $installer = New-Object -ComObject 'Microsoft.Update.Installer'
-			    $installer.Updates = $updatesToInstall        
-			    try{
-			        $installResult = $installer.Install()
-			    }
-			    catch{
-                    "Install Failed"
-			    }
-			    Clear-Variable updatesToInstall -Force -ErrorAction SilentlyContinue
-		    }
-		    Clear-Variable updatesToDownload -Force -ErrorAction SilentlyContinue
-	    }
-	    else{
-		    $updatesToInstall = New-Object -ComObject 'Microsoft.Update.UpdateColl'
-		    $updatesToInstall.Add($entry) | Out-Null 
+        $cond=$false
+        foreach ($record in $UpdateHistory){
+            if($entry.Identity.updateID -eq $record.UpdateIdentity.updateID -and $record.ResultCode -eq 4){
+                $cond=$true
+            }
+        }
+        if($cond){
+            $FailedUpdates += $entry
+        }
+    }
 
-            $installer = New-Object -ComObject 'Microsoft.Update.Installer'
-            $installer.Updates = $updatesToInstall        
-		    try{
-        	    $installResult = $installer.Install()
-		    }
-		    catch{
-                "Install Failed"
-		    }
-		    Clear-Variable updatesToInstall -Force -ErrorAction SilentlyContinue
-	    }
+    if($FailedUpdates.count -ne 0){
+        return "Updates Failed"
     }
-    $Sysinfo = New-Object -ComObject Microsoft.Update.SystemInfo
-    $pending = $Sysinfo.RebootRequired
-    if ($pending) {
-        try{
-	        shutdown.exe /r /f /t 120
-        }
-        catch{
-            exit 1
-        }
-    }
+    return "Updates Available"
 }
-exit
+else{
+    return "Up to Date"
+}
