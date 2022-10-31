f2cdfd547b0cb8b508044f8d3d57ee13e9e671ea
wleksono <108651008+wleksono@users.noreply.github.com>
2022-10-31 11:18:47 +1100

    Add files via upload

---
+++ WSUS/GetFailedKBApplicable.ps1
@@ -0,0 +1,83 @@
+$timeout = 120 ## seconds
+
+#script block for background job
+$FailedUpdates = {
+    $LogFilePath = "C:\Temp\ws1"
+    if (!(Test-Path -Path $LogFilePath))
+    {
+        New-Item -Path $LogFilePath -ItemType Directory | Out-Null
+    }
+    
+    $Logfile = $LogFilePath+"\failedUpdates.log"
+    
+    Function Log([string]$level, [string]$logstring)
+    {
+        $rightSide = [string]::join("   ", ($level, $logstring))
+    
+        $date = Get-Date -Format g
+        $logEntry = [string]::join("    ", ($date, $rightSide)) 
+        Add-content $Logfile -value $logEntry
+    }
+
+    $Session = [activator]::CreateInstance([type]::GetTypeFromProgID("Microsoft.Update.Session"))#,$Computer))
+    $UpdateSearcher = $Session.CreateUpdateSearcher()
+    $updateSearcher.ServerSelection = "1"
+    $TotalHistoryCount = $UpdateSearcher.GetTotalHistoryCount()
+    $UpdateHistory = $UpdateSearcher.QueryHistory(0,$TotalHistoryCount)
+
+    $Criteria = "IsHidden=0 and IsInstalled=0 and IsAssigned=1"
+
+    try{
+        $SearchResult = $UpdateSearcher.Search($Criteria).Updates
+    }
+    catch{
+        Log "Error" "$($_.Exception)"
+        return "Update Search Failed"
+    }
+
+    $FailedUpdates = @()
+
+    if($SearchResult.count -ne 0){
+        foreach ($entry in $SearchResult){
+            $cond=$false
+            foreach ($record in $UpdateHistory){
+                if($record.Date -gt (Get-Date).AddDays(-2) -and $entry.Identity.updateID -eq $record.UpdateIdentity.updateID -and $record.ResultCode -eq 4){
+                    $cond = $true
+                }
+            }
+            if($cond){
+                $FailedUpdates += "KB" + $entry.KBArticleIDs
+            }
+        }
+    
+        $OutputResult = ""
+    
+        if($FailedUpdates.count -ne 0){
+            foreach ($list in $FailedUpdates){
+                $OutputResult += $list + " "
+            }
+            return $OutputResult
+        }
+        return "No Failed Applicable Updates"
+    }
+    else{
+        return "No Missing Updates"
+    }
+}
+
+#start the background job
+$job = Start-Job -ScriptBlock $FailedUpdates
+
+#retrieve job data after timeout
+if ((Wait-Job $job -Timeout $timeout) -ne $null) {
+    #get current job result
+    Receive-Job $job
+}
+
+else{
+    #force removing the job after timeout
+    Remove-Job -force $job
+
+    #return timeout if no result returned before
+    return "action timed out"
+}
---
+++ WSUS/GetInstalledKB.ps1
@@ -0,0 +1,91 @@
+$timeout = 120 ## seconds
+
+#script block for background job
+$InstalledUpdates = {
+    $LogFilePath = "C:\Temp\ws1"
+    if (!(Test-Path -Path $LogFilePath))
+    {
+        New-Item -Path $LogFilePath -ItemType Directory | Out-Null
+    }
+    
+    $Logfile = $LogFilePath+"\installedUpdates.log"
+    
+    Function Log([string]$level, [string]$logstring)
+    {
+        $rightSide = [string]::join("   ", ($level, $logstring))
+    
+        $date = Get-Date -Format g
+        $logEntry = [string]::join("    ", ($date, $rightSide)) 
+        Add-content $Logfile -value $logEntry
+    }
+
+    $Session = [activator]::CreateInstance([type]::GetTypeFromProgID("Microsoft.Update.Session"))#,$Computer))
+    $UpdateSearcher = $Session.CreateUpdateSearcher()
+    $updateSearcher.ServerSelection = "1"
+    $Criteria = "IsHidden=0 and IsInstalled=1"
+
+    try{
+        $SearchResult = $UpdateSearcher.Search($Criteria).Updates
+    }
+    catch{
+        Log "Error" "$($_.Exception)"
+        return "Update Search Failed"
+    }
+
+    $HistoryDays = -28
+
+    $InstalledUpdates = @()
+
+    foreach ($entry in $SearchResult){
+        if($entry.LastDeploymentChangeTime -gt (Get-Date).AddDays($HistoryDays)) {
+            $InstalledUpdates += "KB" + $entry.KBArticleIDs
+        }
+    }
+
+    #Get Windows Updates from WMI
+    $WMIKBs = Get-WmiObject win32_quickfixengineering | ?{($_.InstalledOn -gt (Get-Date).AddDays($HistoryDays)) } | Select-Object HotFixID -ExpandProperty HotFixID
+
+    #Get Windows Updates from DISM
+    $DISMKBList = Get-WindowsPackage -online | ?{$_.InstallTime -gt (Get-Date).AddDays($HistoryDays)} | findstr KB 
+  
+    $pattern = '(?<=KB).+?(?=~)'
+    if($DISMKBList){
+        $DISMKBNumber = [regex]::Matches($DISMKBList, $pattern).Value
+    }
+
+    $DISMKBNumbers = @()
+    ForEach ($Number in $DISMKBNumber) {
+        $DISMKBNumbers += "KB$($Number)"
+    }
+
+    $OutputUpdates = ($InstalledUpdates + $WMIKBs + $DISMKBNumbers) | Sort-Object -Unique
+
+    $OutputResult = ""
+
+    if ($OutputUpdates.count -ne 0){
+        foreach ($list in $OutputUpdates){
+            $OutputResult += $list + " "
+        }
+        return $OutputResult
+    }
+    else{
+        return "No Updates Installed"
+    }
+}
+
+#start the background job
+$job = Start-Job -ScriptBlock $InstalledUpdates
+
+#retrieve job data after timeout
+if ((Wait-Job $job -Timeout $timeout) -ne $null) {
+    #get current job result
+    Receive-Job $job
+}
+
+else{
+    #force removing the job after timeout
+    Remove-Job -force $job
+
+    #return timeout if no result returned before
+    return "action timed out"
+}
